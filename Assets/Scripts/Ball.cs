63d4b4ea765ee89c93d5115be96f8749f5a950f8
jokorkko <94602273+jokorkko@users.noreply.github.com>
2025-06-02 10:47:57 +0300

    Refactoring & Bugfixes
    Refactored the code and made some fixes to prevent possible bugs. Also started to implement the bounce effect on walls, however this doesn't work correctly yet.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -18,7 +18,7 @@ public class Ball : MonoBehaviour
     public Vector3 mouse_world_position;
     public Vector3 original_ball_position;
 
-    //public GameManager game_manager;
+    public GameManager game_manager;
     public UIBuilder ui;
     public int score = 0;
     public int high_score = 0;
@@ -33,11 +33,10 @@ public class Ball : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         original_ball_position = rb.transform.position;
         ui = GetComponent<UIBuilder>();
+        game_manager = GetComponent<GameManager>();
 
         left_wall = GameObject.Find("LeftWall");
         right_wall = GameObject.Find("RightWall");
-
-        //Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
 
         float distance = Vector3.Distance(Camera.main.transform.position, transform.position);
 
@@ -45,7 +44,7 @@ public class Ball : MonoBehaviour
         Vector3 right_position = camera.ViewportToWorldPoint(new Vector3(1, 0, distance));
 
         left_wall.transform.position = left_position;
-        right_wall.transform.position = right_position;//Toteuta kimpoaminen
+        right_wall.transform.position = right_position;
     }
 
     // Update is called once per frame
@@ -60,63 +59,75 @@ public class Ball : MonoBehaviour
             transform.position = original_ball_position;
         }
 
-
-
-        /*if (pos.x < -0.3 || 1.3 < pos.x)
-        {
-            rb.linearVelocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            transform.position = original_ball_position;
-        }*/
-
     }
 
 
 
     void OnMouseDown()
     {
+        LayerMask layerMask = LayerMask.GetMask("Ball_layer");
+
         mouse_on_screen = Input.mousePosition;
+
+        // Ray from camera to mouse position on screen
         Ray ray = Camera.main.ScreenPointToRay(mouse_on_screen);
 
-        if (Physics.Raycast(ray, out RaycastHit hitData))
+        // Check if the created ray hits something on the "Ball_layer", in this case the Ball. So, if the ball is kicked, continue forward.
+        if (Physics.Raycast(ray, out RaycastHit hitData, layerMask))
         {
             mouse_world_position = hitData.point;
+
+            Vector3 ball_location = transform.position;
+
+            // Direction of the kick offset from the center of the ball
+            Vector3 direction = (ball_location - mouse_world_position).normalized;
+
+            // Reset the movement before adding the kick impact to the ball
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            // The impact of the kick: Combination of force originated from kick offset and extra force making ball go upwards to ease the game and prevent downwards kick
+            Vector3 force = direction * DIRECTIONAL_KICK_FORCE + Vector3.up * UPWARDS_KICK_FORCE;
+
+            rb.AddForce(force, ForceMode.Impulse);
+
+            // Add spin for the ball
+            Vector3 torqueDir = Vector3.Cross(Vector3.up, direction);
+            rb.AddTorque(torqueDir * SPIN_FORCE, ForceMode.Impulse);
+
+            game_manager.UpdateScore();
+
         }
 
-        Vector3 ball_location = transform.position;
-
-        // Direction of the kick offset
-        Vector3 direction = (ball_location - mouse_world_position).normalized;
-
-        // Reset the movement
-        rb.linearVelocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-
-        // The impact of the kick: Combination of force originated from kick offset and extra force making ball go upwards to ease the game and prevent downwards kick
-        Vector3 force = direction * DIRECTIONAL_KICK_FORCE + Vector3.up * UPWARDS_KICK_FORCE;
-
-
-        rb.AddForce(force, ForceMode.Impulse);
-
-        // Add spin for the ball
-        Vector3 torqueDir = Vector3.Cross(Vector3.up, direction);
-        rb.AddTorque(torqueDir * SPIN_FORCE, ForceMode.Impulse);
-
-        score += 1;
-        if (score > high_score)
-        {
-            high_score = score;
-            ui.UpdateBestScore(high_score);
-        }
-
-        ui.UpdateScore(score);
 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        score = 0;
-        ui.UpdateScore(score);
+        if (collision.collider.name == "Field")
+        {
+            game_manager.ResetScore();
+        }
+
+        //Ball bounce on the walls, BUGBUGBUG
+        if (collision.collider.name == "LeftWall" || collision.collider.name == "RightWall")
+        {
+            Debug.Log(collision.collider.name);
+
+            // Get the normal of the collision
+            Vector3 normal = collision.contacts[0].normal;
+
+            // Calculate the reflected velocity using Vector3.Reflect
+            Vector3 reflectedVelocity = Vector3.Reflect(rb.linearVelocity, normal);
+            Vector3 reflectedAngularVelocity = Vector3.Reflect(rb.angularVelocity, normal);
+
+            // Apply the reflected velocity to the Rigidbody
+            //rb.linearVelocity = reflectedVelocity * rb.linearVelocity.magnitude;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(reflectedVelocity, ForceMode.VelocityChange);
+            rb.AddTorque(reflectedAngularVelocity, ForceMode.VelocityChange);
+        }
     }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,16 +3,44 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    public int score;
+    private int score;
+    private int high_score;
+    public UIBuilder ui;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;
+        high_score = 0;
+
+        ui = GetComponent<UIBuilder>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void UpdateScore()
+    {
+        score += 1;
+
+        if (score > high_score)
+        {
+            high_score = score;
+            ui.UpdateBestScore(high_score);
+        }
+
+        ui.UpdateScore(score);
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        ui.UpdateScore(score);
+    }
+    
+
 }
